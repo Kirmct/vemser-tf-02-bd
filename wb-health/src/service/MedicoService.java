a857c7d6da350089956a89dd3b65f044b272f74f
RenanBilhan <renan.bilhan@hotmail.com>
2023-07-29 22:49:45 -0300

    Limpeza classe medico e correlatas

--- wb-health/src/service/MedicoService.java
+++ wb-health/src/service/MedicoService.java
@@ -1,12 +1,8 @@
 package service;
-
-import model.Hospital;
 import model.Medico;
-import model.Paciente;
 import model.exceptions.BancoDeDadosException;
 import repository.MedicoRepository;
 import util.CoresMenu;
-
 import java.util.List;
 
 public class MedicoService{
@@ -72,32 +68,5 @@ public class MedicoService{
         } catch (BancoDeDadosException e) {
             e.printStackTrace();
         }
-
     }
-//
-//    private MedicoRepository medicoRepository = new MedicoRepository();
-//
-//    public void inserir(Hospital hospital, Medico medico) {
-//        medicoRepository.cadastrar(hospital, medico);
-//    }
-//
-//    public void listarTodos(Hospital hospital) {
-//        medicoRepository.listarTodos(hospital);
-//    }
-//
-//    public void listarPeloId(Hospital hospital, Integer id) {
-//        medicoRepository.listarPeloId(hospital, id);
-//    }
-//
-//    public void alterarPeloId(Hospital hospital, Integer id, Medico medicoAtualizado){
-//        medicoRepository.alterarPeloId(hospital, id, medicoAtualizado);
-//    }
-//
-//    public void deletarPeloId(Hospital hospital, Integer id){
-//        medicoRepository.deletarPeloId(hospital, id);
-//    }
-//
-//    public Medico buscarId(Hospital hospital, Integer id){
-//        return medicoRepository.buscarId(hospital.getMedicos(), id);
-//    }
 }
