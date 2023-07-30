dcf7a1153af0949760423520f500d3e27ece34ab
RenanBilhan <renan.bilhan@hotmail.com>
2023-07-29 22:04:21 -0300

    start branch feat/renan

---
+++ wb-health/src/tests/MedicoTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import util.MenuMedico;
+
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MedicoTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MenuMedico.deletarPeloId(sc);
+//        MenuMedico.listar();
+
+//        MenuMedico.alterarPeloId(sc);
+
+//        System.out.println("Insira um id: ");
+//        MenuMedico.listarPeloID(sc);
+
+//        System.out.println("Insira um medico: ");
+//        MenuMedico.inserir(sc);
+//    }
+
+
+
+    }
+}
